2c6e827ba60767f84cfa6a7ab043636be3f9bbc9
Emery-S <112206035+Emery-S@users.noreply.github.com>
2023-02-12 22:38:41 -0500

    Earthquakes_past7days

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -19,28 +19,17 @@ let baseMaps = {
 }
 
 let map = L.map('mapid', {
-  center: [43.7, -79.3],
-  zoom: 11,
+  center: [41, -35],
+  zoom: 3,
   layers: [streets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-
-// Accessing the Toronto airline routes GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/Emery-S/Mapping_Earthquakes/main/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
-
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-  console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data, {
-    color: 'blue',
-    fillColor: "yellow",
-    weight: 1,
-    onEachFeature: function(feature, layer) {
-    console.log(feature);
-    layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + '</h3>')}
-  }).addTo(map)
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
 });
+
